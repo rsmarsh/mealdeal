90a8c8ec8177c017e34b5e4016bf8d5d57c1d236
Richard Marshall <missingmarshall@gmail.com>
2020-02-05 18:39:31 +0000

    added material-ui component

--- src/components/MiniDeal.js
+++ src/components/MiniDeal.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import Button from '@material-ui/core/Button';
+
 const MiniDeal = (props) => {
     return (
         <div>
             <h2>{props.title}</h2>
             <h3>{props.venue}</h3>
             <p>{props.description}</p>
+            <Button variant="contained" color="primary">
+                View Deal
+            </Button>
+
         </div>
     )
 };

f3747c0c8dad298f69ced05230e85dc5b93a009e
Richard Marshall <missingmarshall@gmail.com>
2020-02-15 16:08:56 +0000

    created a deal creation form using formik and material-ui components

--- client/src/components/CreateDeal.js
+++ client/src/components/CreateDeal.js
@@ -1,7 +1,101 @@
 import React from 'react';
+import { Formik, Form, Field } from 'formik';
 
-const CreateDeal = () => {
-    return <div>CreateDeal</div>;
+import { Typography, Button, LinearProgress } from '@material-ui/core';
+import { TextField } from 'formik-material-ui';
+
+import './CreateDeal.css';
+
+class CreateDeal extends React.Component {
+    
+    handleFormSubmit(values) {
+        console.log("form submission attempted");
+        console.log(values);
+    }
+
+    renderLoadingBar(isSubmitting) {
+        if (isSubmitting) {
+            return <LinearProgress className/>;
+        } else {
+            return null;
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Typography variant="h5">
+                    Fill in the form to add a new deal to the catalogue
+                </Typography>
+
+                <Formik
+                    initialValues={{
+                        dealTitle: '',
+                        dealDescription: '',
+                        dealVenue: '',
+                        dealLocation: ''
+                    }}
+                    onSubmit={this.handleFormSubmit}
+                >
+                    {({ submitForm, isSubmitting }) => (
+
+                        <Form className="create-deal">
+                            {/* Deal Title */}
+                            <Field
+                                component={TextField}
+                                name="dealTitle"
+                                type="text"
+                                label="Deal Title"
+                                className="form-input"
+                            />
+                            <br />
+
+                            {/* Deal Description */}
+                            <Field
+                                component={TextField}
+                                name="dealDescription"
+                                type="text"
+                                label="Description"
+                                className="form-input"
+                            />
+                             <br />
+
+                            {/* Deal Venue */}
+                            <Field
+                                component={TextField}
+                                name="dealVenue"
+                                type="text"
+                                label="Venue Name"
+                                className="form-input"
+                            />
+                             <br />
+
+                            {/* Deal Location */}
+                            <Field
+                                component={TextField}
+                                name="dealLocation"
+                                type="text"
+                                label="Town/City"
+                                className="form-input"
+                            />
+                            <br />
+                            {this.renderLoadingBar(isSubmitting)}
+                            <Button
+                                className="submit"
+                                variant="contained"
+                                color="primary"
+                                disabled={isSubmitting}
+                                onClick={submitForm}
+                            >
+                                Submit deal
+                            </Button>
+                        </Form>
+                    )}
+                </ Formik>
+            </div>
+        )
+    }
+    
 }
 
 export default CreateDeal;

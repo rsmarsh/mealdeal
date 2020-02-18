b4091b0931cbd31cc9e7d1f8ca6ba0edf0c5adae
Richard Marshall <missingmarshall@gmail.com>
2020-02-18 19:43:33 +0000

    initialised the backend express server boilerplate

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
 require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => res.send('mealdeal backend api'));
+
+
+app.listen(port, () => console.log(`server listening on port ${port}`));

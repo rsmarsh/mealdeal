aba69e7facc025339b9eca3a4df2c0266878d069
Richard Marshall <missingmarshall@gmail.com>
2020-02-22 12:53:40 +0000

    route updates to receive a new deal POST request (no DB implementation yet)

--- server/routes/deal.js
+++ server/routes/deal.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+
+// Retieve a deal
 router.get('/:id', (req, res) => {
     const dealId = req.params.id;
 
     res.status(200).json({deal: dealId});
 });
+
+// Create a new deal
+router.post('/create', (req, res) => {
+
+    console.log("reveiced the following new deal:");
+    console.log(req.body);
+
+    res.status(200).json({success: true});
+
+})
+
 
 module.exports = router;

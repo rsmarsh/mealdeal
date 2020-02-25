891690a5bed9a0091a52e951585b512124e3050a
Richard Marshall <missingmarshall@gmail.com>
2020-02-25 18:03:35 +0000

    added credentials file using env variables to not reveal sensitive logins

---
+++ server/credentials/database.js
@@ -0,0 +1,8 @@
+const dbCredentials = {
+    host:  process.env.DB_HOST || "HOSTNAME",
+    databaseName: process.env.DB_NAME || "DB_NAME",
+    user: process.env.DB_USER || "DBUSERNAME",
+    password: process.env.DB_USER || "DBPASSWORD"
+};
+
+export default dbCredentials; 

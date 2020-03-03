3f90f093a87004c271da069b6bd291f8fda1a62b
Richard Marshall <missingmarshall@gmail.com>
2020-03-03 19:45:48 +0000

    imported compose to allow redux dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore (reducers, applyMiddleware(thunk));
+// enables the use of the redux devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore (
+    reducers, 
+    composeEnhancers(applyMiddleware(thunk))
+);
 
 
 

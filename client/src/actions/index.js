c2eedccc1da7b840e3922d67a47aa414632709d7
Richard Marshall <missingmarshall@gmail.com>
2020-03-11 19:46:33 +0000

    added todo tags and updated todo list

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -10,13 +10,12 @@ export const fetchDeals = () => async (dispatch, getState) => {
     });
     
 }
-
+// DONEXT parse the CREATE_DEAL response from the reducer
 export const createDeal = formValues => async (dispatch, getState) => {
     
     const response = await dealsAPI.create(formValues);
-
     // for scenarios where the server failed to response or an error occured, the data property may be undefined
-    const resData = response?.data || false;
+    const resData = response?.data?.success || false;
 
     dispatch({
         type: 'CREATE_DEAL',

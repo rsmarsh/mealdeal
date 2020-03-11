ab5e9afb27a6fc9f5e77232f48b49ead433a7e25
Richard Marshall <missingmarshall@gmail.com>
2020-03-11 19:42:21 +0000

    created various routes for deal manipulation

--- server/routes/deal.js
+++ server/routes/deal.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const Deal = require('../database/deals.model.js');
 
-
+// All these routes are using a basepath of HOST/deals
 // Retieve a deal
 router.get('/:id', (req, res) => {
     const dealId = req.params.id;
@@ -12,12 +13,43 @@ router.get('/:id', (req, res) => {
 // Create a new deal
 router.post('/create', (req, res) => {
 
+    const resultsCallback = (dbRes) => {
+    
+        res.status(200).json({
+            success: dbRes.warningCount === 0,
+            data: {
+                dealId: dbRes.insertId
+            }
+        });
+    } 
     console.log("received the following new deal:");
     console.log(req.body);
 
-    res.status(200).json({success: true});
+    // TODO enter this deal into the DB
+    const deal = new Deal({...req.body, resultsCallback});
 
-})
+    // insert this deal into the database
+    deal.insert();
+
+
+});
+
+// TODO add delete DB functionality
+router.delete('/:id', (req, res) => {
+    console.log("received a deal delete request");
+});
+
+// TODO add put DB functionality
+router.put('/:id', (req, res) => {
+    console.log(`received a put request to update deal ${req.parmas.id}`);
+    console.log(req.body);
+});
+
+// TODO add patch functionality
+router.patch('/:id', (req, res) => {
+    console.log(`receives a patch request to partially update deal ${req.params.id}`);
+    console.log(req.body);
+});
 
 
 module.exports = router;
